539a7f1b11f26d8cce8c1a500cc4b603a8c1f1da
kashifaxioned <kashifa@axioned.com>
2022-03-31 11:11:38 +0530

    JS functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,13 @@
 
 */
 
+document.querySelector('input[type="range"]').addEventListener("mousemove", function() {handleChange(this.value)})
+let rangeLabel = document.querySelector("label");
+
+function handleChange(x) {
+  rangeLabel.innerHTML = x
+  rangeLabel.style.left = (x-10) + "%"
+}
 
 
 
